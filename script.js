896cc5ddaab28ff9d74df774f8db8181ab6ac2c7
gurh4n <g@gorduk.org>
2021-01-26 19:53:37 +0300

    deleted

--- script.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    $(".scrolling").click(function() {
-        // remove classes from all
-        $(".scrolling").removeClass("active");
-        // add class to the one we clicked
-        $(this).addClass("active");
-    });
-});
